a927c87e6eaeaa75ed8b7dcaac8ccd9bca616309
aminpaw <rocketlee2004@gmail.com>
2023-04-26 20:48:27 +0200

    finish fourier

--- fourier/fourier.js
+++ fourier/fourier.js
@@ -2,23 +2,72 @@ let time = 0;
 let wave = [];
 
 function setup(){
-    createCanvas(600, 400);
+    createCanvas(window.innerWidth*0.8, window.innerHeight);
+    // move canvas to center of any screen
+    canvas = document.querySelector('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.left = (window.innerWidth - width) / 2 + 'px';
+    //canvas.style.top = (window.innerHeight - height) / 2 + 'px';
+    canvas.style.zIndex = -1;
+
+    // move slider below canvas 
+    slider = document.getElementById("myRange");
+    slider.style.position = 'absolute';
+    slider.style.width = window.innerWidth*0.8 + 'px';
+    slider.style.left = (window.innerWidth - width) / 2 + 'px';
+    slider.style.top = window.innerHeight*1.1 + height/5 + 'px';
+    slider.style.zIndex = 1;
+
+    let k = slider.value; // Display the default slider value
+
+
+
 }
 
 function draw(){
     background(0);
-    translate(200, 200);
+    translate(width/3, height/2);
+    k = slider.value; // Display the default slider value
+    //update printed k value
+    document.getElementById("k_val").innerHTML = "K= " + k;
+    // Update the current slider value (each time you drag the slider handle)
+    let kViz = document.getElementById("k_val");
+    //move text to left upper corner of canvas
+    kViz.style.position = 'absolute';
+    kViz.style.left = (window.innerWidth - width*0.9) / 2 + 'px';
+    kViz.style.top = (window.innerHeight - height*0.5) / 2 + 'px';
+    kViz.style.zIndex = 1;
 
-    let radius = 100;
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < k; i++){
+        n = 2*i + 1;
+        prevX = x;
+        prevY = y;
+        let radius = (height/4) * (4 / (n * PI));
+        stroke(255);
+        noFill();
+        ellipse(prevX, prevY, radius * 2);
+        x += radius * cos(n*time);
+        y += radius * sin(n*time);
+        stroke(255, 75);
+        line(prevX, prevY, x, y);
+        fill(255);
+        ellipse(x,y,5);
+    }
+    wave.unshift(y);
+    line(x , y, width/3, wave[0]);
+    beginShape();
+    translate(width/3, 0);
     stroke(255);
     noFill();
-    ellipse(0, 0, radius * 2);
-    let x = radius * cos(time);
-    let y = radius * sin(time);
-
-    //line(0, 0, x, y);
-    fill(255);
-    ellipse(x,y,8);
-
+    for (let i = 0; i < wave.length; i++){
+        vertex(i, wave[i]);
+    }
+    endShape();
+    if (wave.length > 500){
+        wave.pop();
+    }
     time += 0.02;
 }
